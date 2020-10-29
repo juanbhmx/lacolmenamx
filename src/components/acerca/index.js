e87fd033c14d443b777cbd1c9538d03150493a70
juanbhmx <67531291+juanbhmx@users.noreply.github.com>
2020-10-29 00:19:55 -0600

    prof

--- src/components/acerca/index.js
+++ src/components/acerca/index.js
@@ -7,12 +7,12 @@ const acerca = () => {
 return (
     <div class="col-sm-12 padding-right">
     <div class="features_items">
-      <h2 class="title text-center">Integrantes</h2>
+      <h2 class="title text-center">Nuestro Equipo</h2>
       <div class="col-sm-4">
         <div class="product-image-wrapper">
           <div class="single-products">
               <div class="productinfo text-center">
-                <img src="assets/images/home/product2.jpg" alt="" />
+                <img src="assets/images/home/prof3.jpg" alt="" />
                 <h2>Brandon Martinez H.</h2>
                 <p>Encargado de:</p>
                 
@@ -22,7 +22,7 @@ return (
           <div class="choose">
             <ul class="nav nav-pills nav-justified">
             <li><a href="#"><i ></i>Email</a></li>
-              <li><a href="#"><i class="fa fa-plus-square"></i>771 243 05 61</a></li>
+              <li><a href="#"><i class="fa fa-plus-square"></i>+52 1 55 6534 2312</a></li>
             </ul>
           </div>
         </div>
@@ -31,7 +31,7 @@ return (
         <div class="product-image-wrapper">
           <div class="single-products">
             <div class="productinfo text-center">
-              <img src="assets/images/home/product2.jpg" alt="" />
+              <img src="assets/images/home/prof1.jpg" alt="" />
               <h2>Juan Bautista H.</h2>
               <p>Encargado de:</p>
              
@@ -41,7 +41,7 @@ return (
           <div class="choose">
             <ul class="nav nav-pills nav-justified">
                 <li><a href="#"><i ></i>Email</a></li>
-              <li><a href="#"><i class="fa fa-plus-square"></i>771 243 05 61</a></li>
+              <li><a href="#"><i class="fa fa-plus-square"></i>+52 1 771 243 05 61</a></li>
             </ul>
           </div>
         </div>
@@ -50,7 +50,7 @@ return (
         <div class="product-image-wrapper">
           <div class="single-products">
             <div class="productinfo text-center">
-              <img src="assets/images/home/product2.jpg" alt="" />
+              <img src="assets/images/home/prof2.jpg" alt="" />
               <h2>Victor Samuel L.</h2>
               <p>Encargado de:</p>
               
@@ -60,7 +60,7 @@ return (
           <div class="choose">
             <ul class="nav nav-pills nav-justified">
             <li><a href="#"><i ></i>Email</a></li>
-              <li><a href="#"><i class="fa fa-plus-square"></i>771 243 05 61</a></li>
+              <li><a href="#"><i class="fa fa-plus-square"></i>+52 1 55 3932 9142</a></li>
             </ul>
           </div>
         </div>
@@ -69,7 +69,7 @@ return (
         <div class="product-image-wrapper">
           <div class="single-products">
             <div class="productinfo text-center">
-              <img src="assets/images/home/product2.jpg" alt="" />
+              <img src="assets/images/home/prof4.jpg" alt="" />
               <h2>Bernardino Osorio C.</h2>
               <p>Encargado de:</p>
               
@@ -81,7 +81,7 @@ return (
           <div class="choose">
             <ul class="nav nav-pills nav-justified">
               <li><a href="#"><i ></i>Email</a></li>
-              <li><a href="#"><i class="fa fa-plus-square"></i>771 243 05 61</a></li>
+              <li><a href="#"><i class="fa fa-plus-square"></i>+52 1 746 118 4139</a></li>
             </ul>
           </div>
         </div>
